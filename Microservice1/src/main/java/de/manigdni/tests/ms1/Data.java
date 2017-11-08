af72d2aca3c1b3e31b83d15867951ace73adf17b
manigdni <manigdni@p01152280.schaeffler.com>
2017-11-08 14:11:01 +0100

    initial commit

---
+++ Microservice1/src/main/java/de/manigdni/tests/ms1/Data.java
@@ -0,0 +1,23 @@
+package de.manigdni.tests.ms1;
+
+public class Data {
+	private long key;
+	private String value;
+
+	public long getKey() {
+		return key;
+	}
+
+	public void setKey(long key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
